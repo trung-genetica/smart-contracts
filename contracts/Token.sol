6ecc86ff62cbf8de3729091df30e349faba1507c
trung.nguyen <trung.nguyen@genefriendway.com>
2024-09-20 11:03:43 +0700

    chore: update deploy contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CustomToken is ERC20, Ownable {
+contract LifePointToken is ERC20, Ownable {
     constructor() ERC20("LifePoint", "LP") {
         _mint(msg.sender, 100000000 * 10 ** decimals()); // 100,000,000
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,7 @@ async function main() {
     console.log("Account balance:", (await deployer.provider.getBalance(deployer.address)).toString());
 
     // Deploy LifePoint token
-    const LifePoint = await ethers.getContractFactory("CustomToken");
+    const LifePoint = await ethers.getContractFactory("LifePointToken");
     const lifePoint = await LifePoint.deploy();
     await lifePoint.deployed();
     console.log("LifePoint deployed to:", lifePoint.address);
@@ -20,13 +20,9 @@ async function main() {
     console.log("TokenLock deployed to:", tokenLock.address);
 
     // Initialize TokenLock contract
-    const depositDeadline = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
-    const lockDuration = 3600 * 24 * 1; // 1 day
     const tx = await tokenLock.initialize(
         deployer.address, // Owner address
         lifePoint.address, // Token address
-        depositDeadline,
-        lockDuration,
         "Locked LifePoint",
         "LLP"
     );
